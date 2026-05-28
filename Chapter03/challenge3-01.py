278cc81ecbe637c09a7fffc0a72ab961cec10f67
vagrant <s26005@std.it-college.ac.jp>
2026-05-28 07:17:46 +0000

    第３章演習終了

---
+++ Chapter03/challenge3-01.py
@@ -0,0 +1,10 @@
+aisatsu = "ohayou"
+
+if aisatsu == "ohayou":
+    print("朝")
+
+elif aisatsu == "konnitiha":
+    print("昼")
+
+elif aisatsu == "konnbannha":
+    print("夜")
---
+++ Chapter03/challenge3-06.py
@@ -0,0 +1,8 @@
+age = 18
+
+if age >= 20:
+    print("20歳以上")
+elif age >= 18:
+    print("18歳以上20歳未満")
+else:
+    print("18歳未満")
